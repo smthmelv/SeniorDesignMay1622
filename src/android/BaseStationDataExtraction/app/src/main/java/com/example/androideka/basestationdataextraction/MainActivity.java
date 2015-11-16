45ab379ff7bfa8cee2548486156b1c363c7e667e
mattrose <mattrose@iastate.edu>
2015-11-16 15:37:56 -0600

    Turn on bluetooth, find and list devices, click on device to connect.

--- src/android/BaseStationDataExtraction/app/src/main/java/com/example/androideka/basestationdataextraction/MainActivity.java
+++ src/android/BaseStationDataExtraction/app/src/main/java/com/example/androideka/basestationdataextraction/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.androideka.basestationdataextraction;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter bluetooth;
+
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> deviceNames = new ArrayList<>();
+    private HashMap<String, BluetoothDevice> devices = new HashMap<>();
+
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action))
+            {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d("NAME", device.getName());
+                Log.d("ADDRESS", device.getAddress());
+                deviceNames.add(device.getName());
+                devices.put(device.getName(), device);
+                adapter.notifyDataSetChanged();
+                //Toast.makeText(context, device.getName(), Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +72,40 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ListView list = (ListView) findViewById(R.id.deviceList);
+        adapter  = new ArrayAdapter<>(this, R.layout.device, deviceNames);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final BluetoothDevice device =
+                        devices.get((String)parent.getItemAtPosition(position));
+                if(device.getName() != null && device.getName().equals("")) // Use name of board
+                {
+                    try {
+                        bluetooth.cancelDiscovery();
+                        long msb = 16;
+                        long lsb = 22;
+                        UUID uuid = new UUID(msb, lsb);
+                        BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuid);
+                        socket.connect();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        bluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(bluetooth != null && !bluetooth.isEnabled())
+        {
+            Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enable, 42);
+        }
+
+        registerReceiver(receiver, filter);
     }
 
     @Override
@@ -48,5 +128,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == 42)
+        {
+            Log.d("BLUETOOTH", "Bluetooth Enabled.");
+        }
+        else
+        {
+            Log.e("BLUETOOTH", "Bluetooth not enabled.");
+        }
+    }
+
+    public void connect(View view)
+    {
+        if(bluetooth.startDiscovery())
+        {
+            Log.d("BLUETOOTH", "Scanning for devices...");
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        unregisterReceiver(receiver);
     }
 }
